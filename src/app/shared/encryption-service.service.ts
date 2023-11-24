9b6847d360ab94cbf274c61ae723de1009e50cf1
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-24 15:50:56 +0200

    switched back to crypto-js, fixed implementation

--- src/app/shared/encryption-service.service.ts
+++ src/app/shared/encryption-service.service.ts
@@ -1,4 +1,4 @@
-import * as crypto from 'crypto';
+import * as CryptoJS from 'crypto-js';
 import { Injectable } from '@angular/core';
 
 import { environment } from '../../environments/environment';
@@ -7,34 +7,24 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class EncryptionService {
-  private secretKey: Buffer = Buffer.from(
-    'vl/VxRuThkD+v+9S7twDR/eT9v+mye2EvaF4ojeRhTM=',
-    'base64'
+  private secretKey = CryptoJS.enc.Base64.parse(
+    'vl/VxRuThkD+v+9S7twDR/eT9v+mye2EvaF4ojeRhTM='
   );
-  private iv: Buffer = Buffer.from('PynS/ydkhb2EUMzVty9sww==', 'base64');
-
-  constructor() {}
+  private iv = CryptoJS.enc.Base64.parse('PynS/ydkhb2EUMzVty9sww==');
 
   encryptQuestion(topic: string): string {
-    // Promise<string> here? idk, I'm sure there's some minor changes to be made.
-    const cipher = crypto.createCipheriv(
-      'aes-256-cbc',
-      this.secretKey,
-      this.iv
-    );
-    let encrypted = cipher.update(topic, 'utf8', 'base64');
-    encrypted += cipher.final('base64');
-    return encrypted;
+    const plaintext = CryptoJS.enc.Utf8.parse(topic);
+    const cipherText = CryptoJS.AES.encrypt(plaintext, this.secretKey, {
+      iv: this.iv,
+    });
+    return cipherText.toString();
   }
 
   decryptAnswer(encryptedAnswer: string): string {
-    const decipher = crypto.createDecipheriv(
-      'aes-256-cbc',
-      this.secretKey,
-      this.iv
-    );
-    let decrypted = decipher.update(encryptedAnswer, 'base64', 'utf8');
-    decrypted += decipher.final('utf8');
+    const bytes = CryptoJS.AES.decrypt(encryptedAnswer, this.secretKey, {
+      iv: this.iv,
+    });
+    const decrypted = bytes.toString(CryptoJS.enc.Utf8);
     return decrypted;
   }
 }
